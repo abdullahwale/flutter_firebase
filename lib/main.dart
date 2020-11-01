1486832ad3222da942b0c760aef5691862db072d
abdullahwale <abdullahwale@gmai.com>
2020-11-01 21:28:02 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,143 @@
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
+import 'package:simple_firbase/model/board.dart';
+import 'package:flutter/material.dart';
+
+final FirebaseDatabase database = FirebaseDatabase.instance;
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<Board> boardMessages = List();
+  Board board;
+  final FirebaseDatabase database = FirebaseDatabase.instance;
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  DatabaseReference databaseReference;
+
+  @override
+  void initState() {
+    super.initState();
+
+    board = Board("", "");
+    databaseReference = database.reference().child("board_app");
+    databaseReference.onChildAdded.listen(_onEntryAdded);
+    databaseReference.onChildChanged.listen(_onEntryChanged);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Board App"),
+      ),
+      body: Column(
+        children: <Widget>[
+          Flexible(
+            flex: 0,
+            child: Center(
+              child: Form(
+                key: formKey,
+                child: Flex(
+                  direction: Axis.vertical,
+                  children: <Widget>[
+                    ListTile(
+                      leading: Icon(Icons.subject),
+                      title: TextFormField(
+                        initialValue: "",
+                        onSaved: (val) => board.subject = val,
+                        validator: (val) => val == "" ? val : null,
+                      ),
+                    ),
+
+                    ListTile(
+                      leading: Icon(Icons.message),
+                      title: TextFormField(
+                        initialValue: "",
+                        onSaved: (val) => board.body = val,
+                        validator: (val) => val == "" ? val : null,
+                      ),
+                    ),
+
+                    //Send or Post button
+                    FlatButton(
+                      child: Text("Post"),
+                      color: Colors.blue,
+                      onPressed: () {
+                        handleSubmit();
+                      },
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Flexible(
+            child: FirebaseAnimatedList(
+              query: databaseReference,
+              itemBuilder: (_, DataSnapshot snapshot,
+                  Animation<double> animation, int index) {
+                return Card(
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      backgroundColor: Colors.blue,
+                    ),
+                    title: Text(boardMessages[index].subject),
+                    subtitle: Text(boardMessages[index].body),
+                  ),
+                );
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  void _onEntryAdded(Event event) {
+    setState(() {
+      boardMessages.add(Board.fromSnapshot(event.snapshot));
+    });
+  }
+
+  void _onEntryChanged(Event event) {
+    var oldEntry = boardMessages.singleWhere((entry) {
+      return entry.key == event.snapshot.key;
+    });
+
+    setState(() {
+      boardMessages[boardMessages.indexOf(oldEntry)] =
+          Board.fromSnapshot(event.snapshot);
+    });
+  }
+
+  void handleSubmit() {
+    final FormState form = formKey.currentState;
+    if (form.validate()) {
+      form.save();
+      form.reset();
+      //save form data to the database
+      databaseReference.push().set(board.toJson());
+    }
+  }
+}
